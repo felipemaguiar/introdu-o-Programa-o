1e98af5a8b8794b364969c848a044f5694451063
felipemaguiar <felipemaguiar@github.com>
2022-09-21 19:40:03 -0300

    ex 10.1

--- Cap_10/Ex 10.1.py
+++ Cap_10/Ex 10.1.py
@@ -1,7 +1,7 @@
 class Televisao:
     def __init__(self, tamanho, marca):
-        self.tamanho = tamanho
-        self.marca = marca
+        self.__tamanho = tamanho
+        self.__marca = marca
 
     def tv(self):
         return f'Marca: {self.__marca} e Tamanho: {self.__tamanho}'
@@ -11,3 +11,17 @@ tv1 = Televisao("32'", 'LG')
 tv2 = Televisao("50'", "samsung")
 print(tv1.tv())
 print(tv2.tv())
+
+# Podemos resolver de outra maneira - tornando a marca e o tamanho público
+
+
+class Televisao2:
+    def __init__(self, tamanho, marca):
+        self.tamanho = tamanho
+        self.marca = marca
+
+
+tv1 = Televisao2("32'", 'LG')
+tv2 = Televisao2("50'", "samsung")
+print(f'Marca {tv1.marca} e Tamanho: {tv1.tamanho}')
+print(f'Marca {tv2.marca} e Tamanho: {tv2.tamanho}')
