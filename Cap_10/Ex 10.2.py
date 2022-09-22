11633979e4e907a1da069da04eb1e426dc0dd5a0
felipemaguiar <felipemaguiar@github.com>
2022-09-22 18:53:44 -0300

    ex 10.2

---
+++ Cap_10/Ex 10.2.py
@@ -0,0 +1,18 @@
+class Televisao:
+    def __init__(self, canal_inicial, min, max):
+        self.ligada = False
+        self.canal = canal_inicial
+        self.cmin = min
+        self.cmax = max
+
+    def muda_canal_para_baixo(self):
+        if self.canal -1 >= self.cmin:
+            self.canal -= 1
+
+    def muda_canal_para_cima(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+
+
+tv = Televisao(8, 1, 99)
+print(tv.canal)
